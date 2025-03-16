fd84990fd646183280c6714391c306699732d334
saji2000 <sajaddaneshmand79@gmail.com>
2025-03-16 13:50:03 -0400

    Learning with types

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,6 +11,14 @@ namespace ConsoleApp1
             Console.WriteLine("Hello World, we are ready, Thank God!");
             Console.WriteLine("I am learning c# praise God");
             Console.WriteLine(3 + 5);
+
+            string name = "Sajad";
+            Console.WriteLine("my name is: " + name);
+
+            int myInt = 10;
+            double myDouble = myInt;
+            
+            Console.WriteLine(Convert.ToString(myDouble));
         }
     }
 }
--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68c8e64038c3ac02cb16955a05ace1f5c887bb27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5443ef09cc704ebf6e989d032c723c66f2dc2c0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
