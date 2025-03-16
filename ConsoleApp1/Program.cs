68c8e64038c3ac02cb16955a05ace1f5c887bb27
saji2000 <sajaddaneshmand79@gmail.com>
2025-03-16 13:40:04 -0400

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World, we are ready, Thank God!");
+        }
+    }
+}
+
