8848505cae55eda5fe9a48051106737fbdc2aefd
saji2000 <sajaddaneshmand79@gmail.com>
2025-03-19 16:42:43 -0400

    Learned while loop, praise God

---
+++ csharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many starts to you want to write: ");
+            int num = Convert.ToInt16(Console.ReadLine());
+            int i = 0;
+            while (i < num)
+            {
+                Console.Write("*");
+                i++;
+            }
+        }
+    }
+}
+
