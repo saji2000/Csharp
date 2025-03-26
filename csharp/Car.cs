0dcda5e75a7123387d900e47044701f20e4122b7
saji2000 <sajaddaneshmand79@gmail.com>
2025-03-25 20:58:56 -0400

    Access in c#, praise God

--- csharp/Car.cs
+++ csharp/Car.cs
@@ -13,11 +13,16 @@ namespace csharp
             brand = carBrand;
             model = carModel;
             year = carYear;
-        }
+        } 
     
         public void DisplayCar()
         {
             Console.WriteLine($"The car's brand is: {brand} and the model is : {model} {year}");
+        }
+
+        private void CarPrice()
+        {
+            Console.WriteLine("50,000");
         }
     }
 }
